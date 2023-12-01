9fbdd2c0eda34bc54c7580a7843a08cbb181fc7e
Gwendolyn Payne <gwenpayne2012@yahoo.com>
2023-11-30 23:36:37 -0500

    add node server

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors"
+import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+const app = express();
+app.use(express.json());
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    })
+  );
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+app.use(
+  session(sessionOptions)
+);
+  
+UserRoutes(app);
+app.listen(process.env.PORT || 4000);
+
---
+++ users/schema.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    first_name: { type: String, required: true},
+    last_name: { type: String, required: true},
+    email: { type: String, required: true},
+    role: {
+      type: String, require: true,
+      enum: ["READER", "PROFESSIONAL", "PUBLISHER"],
+      default: "READER" },
+  },
+  { collection: "users" });
+export default userSchema;
